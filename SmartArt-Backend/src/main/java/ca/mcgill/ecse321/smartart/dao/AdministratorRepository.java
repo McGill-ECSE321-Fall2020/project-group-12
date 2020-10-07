1e32ce87307d175ad4fa93535c0de6db6bf1b58c
Minh Quan Hoang <minh.q.hoang@mail.mcgill.ca>
2020-10-07 00:33:15 -0400

    Javadoc changes

--- SmartArt-Backend/src/main/java/ca/mcgill/ecse321/smartart/dao/AdministratorRepository.java
+++ SmartArt-Backend/src/main/java/ca/mcgill/ecse321/smartart/dao/AdministratorRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.smartart.model.Administrator;
 
+/**
+ * 
+ * @author Group 12
+ * Interface used as a template for a repository of administrators.
+ */
 public interface AdministratorRepository extends CrudRepository<Administrator, String>{
 
 	Administrator findAdministratorByEmail(String email);
