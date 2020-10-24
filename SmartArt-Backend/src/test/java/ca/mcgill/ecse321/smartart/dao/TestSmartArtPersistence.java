ca060eef41405d2d20681cb6d241b12e4efad1f6
aidanwilliams09 <aidan19@me.com>
2020-10-24 16:28:06 -0400

    fixed error where application would fail to merge copies of entities

--- SmartArt-Backend/src/test/java/ca/mcgill/ecse321/smartart/dao/TestSmartArtPersistence.java
+++ SmartArt-Backend/src/test/java/ca/mcgill/ecse321/smartart/dao/TestSmartArtPersistence.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
