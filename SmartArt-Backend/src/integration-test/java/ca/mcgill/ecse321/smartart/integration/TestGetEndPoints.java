3e59d25ef83e0b3d170d99f9e54187c338134bae
Ben Macnaughton <robert.macnaughton@mail.mcgill.ca>
2020-10-27 19:42:50 -0400

    started integration tests

---
+++ SmartArt-Backend/src/integration-test/java/ca/mcgill/ecse321/smartart/integration/TestGetEndPoints.java
@@ -0,0 +1,75 @@
+package ca.mcgill.ecse321.smartart.integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.sql.Date;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import ca.mcgill.ecse321.smartart.dao.*;
+import ca.mcgill.ecse321.smartart.service.SmartArtService;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TestGetEndPoints {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	private HttpHeaders headers = new HttpHeaders();
+	
+	@Autowired
+	private ArtistRepository artistRepository;
+	@Autowired
+	private AdministratorRepository administratorRepository;
+	@Autowired
+	private BuyerRepository buyerRepository;
+	@Autowired
+	private GalleryRepository galleryRepository;
+	@Autowired
+	private PostingRepository postingRepository;
+	@Autowired
+	private PurchaseRepository purchaseRepository;
+	
+	@Autowired
+	private SmartArtService service;
+	
+	@AfterEach
+	public void clearDatabase() {
+		galleryRepository.deleteAll();
+		administratorRepository.deleteAll();
+		artistRepository.deleteAll();
+		buyerRepository.deleteAll();
+		postingRepository.deleteAll();
+		purchaseRepository.deleteAll();
+	}
+	
+	@BeforeEach
+	public void setUpTests() {
+		service.createGallery("Gallery", "Montreal", 0.05);
+	}
+	
+	@Test
+	public void getGalleries() {
+		ResponseEntity<String> response = restTemplate.exchange("http://localhost:8080/galleries", HttpMethod.GET, null, String.class);
+		// Check Status
+	    assertEquals(HttpStatus.OK, response.getStatusCode());
+	    String result = response.getBody().toString();
+	    assertTrue(result.contains("[{\"name\":\"Gallery\",\"city\":\"Montreal\",\"commission\":0.05}]"));
+	}
+	
+}
