a9e987001a4b319fa530fb86bf050e1ee1f258e4
gregorywalfish <gregory.walfish@mail.mcgill.ca>
2020-11-09 12:35:04 -0500

    first tutorial completed

---
+++ SmartArt-Frontend/src/components/SmartArt.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="smartart">
+    <h2>People</h2>
+    <table>
+      <tr>
+          <td>John</td>
+          <td>Art to buy</td>
+      </tr>
+      <tr>
+          <td>
+              <input type="text" placeholder="Person Name">
+          </td>
+          <td>
+              <button>Create</button>
+          </td>
+      </tr>
+    </table>
+    <p>
+      <span style="color:red">Error: Message text comes here</span>
+    </p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+  #smartart {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    background: #f2ece8;
+  }
+</style>
