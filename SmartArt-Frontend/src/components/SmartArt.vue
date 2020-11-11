366572a60e3e5531fa23d39b22aae422726e3f26
aidanwilliams09 <aidan19@me.com>
2020-11-11 15:10:15 -0500

    backend handling updated

--- SmartArt-Frontend/src/components/SmartArt.vue
+++ SmartArt-Frontend/src/components/SmartArt.vue
@@ -1,24 +1,69 @@
 <template>
   <div id="smartart">
-<!--    <h3>-->
-<!--      Sign Up or Login-->
-<!--    </h3>-->
-<!--    <table>-->
-<!--      <tr>-->
-<!--        <td>-->
-<!--          <button>Sign Up</button>-->
-<!--        </td>-->
-<!--        <td>-->
-<!--          <button>Login</button>-->
-<!--        </td>-->
-<!--      </tr>-->
-<!--    </table>-->
+    <img src="../assets/SmartArt.png">
+    <PostingList v-bind:postingList="postingList"/>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import PostingList from './PostingList'
+var config = require('../../config')
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
 export default {
-  name: "SmartArt.vue"
+  name: "SmartArt",
+  city: "Montreal",
+  commission: '',
+  data () {
+    return {
+      artists: [],
+      buyers: [],
+      admins: [],
+      postingList: [],
+      errorArtist: '',
+      errorBuyer: '',
+      errorPosting: '',
+      errorAdmin: '',
+      response: []
+    }},
+    components: {
+      PostingList
+    },
+    created: function () {
+      AXIOS.get('/artists').then(response => {
+        this.artists = response.data
+      })
+      .catch(e => {
+        this.errorArtist = e
+      })
+      AXIOS.get('/buyers').then(response => {
+        this.buyers = response.data
+      })
+        .catch(e => {
+          this.errorBuyer = e
+        })
+      AXIOS.get('/administrators').then(response => {
+        this.admins = response.data
+      })
+        .catch(e => {
+          this.errorAdmin = e
+        })
+      AXIOS.get('/postings').then(response => {
+        this.postingList = response.data
+      })
+        .catch(e => {
+          this.errorPosting = e
+        })
+    }
+
 }
 </script>
 
