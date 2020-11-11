acc379779df1682b1fe50818c9c60be3f46be139
aidanwilliams09 <aidan19@me.com>
2020-11-11 00:43:48 -0500

    rough initial components created and some examples of how to create components

---
+++ SmartArt-Frontend/src/components/PostingItem.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <h1>
+    {{posting.title}}
+  </h1>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "PostingItem",
+  props: [
+    "posting"
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ SmartArt-Frontend/src/components/PostingList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-bind:key="posting.id" v-for="posting in postingList">
+      <PostingItem v-bind:posting="posting"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostingItem from "./PostingItem"
+
+export default {
+  name: "PostingList",
+  // components are the data we need from imports, can be referenced in our data function
+  components: {
+    PostingItem
+  },
+  // props "exports" what's inside so that whatever imports PostingList has access to data inside
+  props: ["postingList"]
+}
+</script>
+
+<style scoped>
+
+</style>
