d85d7b785001803cbeccfba762015e1ab6a9de10
aidanwilliams09 <aidan19@me.com>
2020-11-11 21:50:54 -0500

    main page rough draft

--- SmartArt-Frontend/src/components/PostingList.vue
+++ SmartArt-Frontend/src/components/PostingList.vue
@@ -1,25 +1,47 @@
 <template>
   <div>
-    <div v-bind:key="posting.id" v-for="posting in postingList">
-      <PostingItem v-bind:posting="posting"/>
-    </div>
+    <b-card-group columns id="postingCardColumn">
+      <b-card id="postingCardClass" v-bind:key="posting.id" v-for="posting in postingList"
+        v-bind:img-src="posting.image"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-title>
+          {{posting.title}}
+        </b-card-title>
+        <b-card-text>
+          <div>
+            ${{posting.price}}
+          </div>
+          {{ posting.description }}
+        </b-card-text>
+
+        <b-button href="#" variant="primary">View Post</b-button>
+        <template #footer>
+          <small class="text-muted">Posted {{posting.date}}</small>
+        </template>
+      </b-card>
+    </b-card-group>
   </div>
 </template>
 
 <script>
-import PostingItem from "./PostingItem"
 
 export default {
   name: "PostingList",
-  // components are the data we need from imports, can be referenced in our data function
-  components: {
-    PostingItem
-  },
   // props "exports" what's inside so that whatever imports PostingList has access to data inside
   props: ["postingList"]
 }
 </script>
 
 <style scoped>
-
+  #postingCardClass {
+    box-shadow: 6px 6px #999999;
+  }
+  #postingCardColumn {
+    padding-bottom: 20px;
+  }
 </style>
