d9a7440a82e7b9128ac2f4aa1985ec52c7640fe8
Ben Macnaughton <robert.macnaughton@mail.mcgill.ca>
2020-11-11 20:11:58 -0500

    started login page

--- SmartArt-Frontend/src/App.vue
+++ SmartArt-Frontend/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
     <img src="./assets/SmartArt.png">
-<!--    v-bind will map a variable postingList to our postingList in the data function
-        essentially displays the data the is put into our data function-->
-    <PostingList v-bind:postingList="postingList"/>
     <router-view></router-view>
   </div>
 </template>
@@ -13,23 +10,6 @@ import PostingList from "./components/PostingList"
 
 export default {
   name: 'app',
-  components: {
-    PostingList
-  },
-  data () {
-    return {
-      postingList: [
-        {
-          id: 1,
-          title: "Posting 1"
-        },
-        {
-          id: 2,
-          title: "Posting 2"
-        }
-      ]
-    }
-  }
 }
 </script>
 
--- SmartArt-Frontend/src/router/index.js
+++ SmartArt-Frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import SmartArt from "../components/SmartArt";
+import Login from "../components/Login";
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/app',
       name: 'SmartArt',
       component: SmartArt
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
