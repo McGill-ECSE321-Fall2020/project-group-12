e3fdd447a635ad3bba6f75004fc5f2d506c9ea66
Ben Macnaughton <robert.macnaughton@mail.mcgill.ca>
2020-11-27 18:20:06 -0500

    improved readability

--- SmartArt-Android/app/src/main/java/ca/mcgill/ecse321/smartart/Login.java
+++ SmartArt-Android/app/src/main/java/ca/mcgill/ecse321/smartart/Login.java
@@ -16,12 +16,11 @@ import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
 
+//Activity class for Login Activity
 public class Login extends AppCompatActivity {
 
-    private String error = "";
     private EditText email;
     private EditText password;
-    private String success = "";
     private String user = "";
 
 
@@ -29,23 +28,32 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Initialize activity layout
         setContentView(R.layout.login);
+        //Bind text box inputs to local variables to be used in login process
         email = findViewById(R.id.email_box);
         password = findViewById(R.id.password_box);
     }
 
+    //Method to login using a call to the backend
     public void login(View v) {
         RequestParams params = new RequestParams();
+        //Add login parameters
         params.add("email", email.getText().toString());
         params.add("password", password.getText().toString());
+        //Backend call to authenticate the login credentials
         HttpUtils.get("android/login", params, new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                //clear fields for next login
                 email.setText("");
                 password.setText("");
                 try {
+                    //store user email locally to be passed to other activities
                     user = response.getString("email");
+                    //Pass email to other activities
                     sendUser();
+                    //Redirect to main method
                     toMain();
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -57,26 +65,24 @@ public class Login extends AppCompatActivity {
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 System.out.println(errorResponse);
                 final TextView displayError = (TextView) findViewById(R.id.login_error);
+                //clear text boxes for next login attempt
                 email.setText("");
                 password.setText("");
                 displayError.append("Login Unsuccessful");
-                try {
-                    error += errorResponse.get("message").toString();
-                } catch (JSONException e) {
-                    error += e.getMessage();
-                }
             }
         });
 
 
     }
 
+    //Method to redirect app to the main activity
     public void toMain(){
         setContentView(R.layout.activity_main);
         Intent intent= new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
+    //Method to send the logged in users email to other activities
     public void sendUser(){
         Intent intent = new Intent(getBaseContext(), MainActivity.class);
         intent.putExtra("USER", user);
