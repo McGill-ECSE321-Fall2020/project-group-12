73cc68a79781632400d078f19f73b04d0ed2efc0
Ben Macnaughton <robert.macnaughton@mail.mcgill.ca>
2020-11-26 21:18:39 -0500

    changes

--- SmartArt-Android/app/src/main/java/ca/mcgill/ecse321/smartart/Login.java
+++ SmartArt-Android/app/src/main/java/ca/mcgill/ecse321/smartart/Login.java
@@ -1,15 +1,41 @@
 package ca.mcgill.ecse321.smartart;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.JsonStreamerEntity;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.entity.StringEntity;
+import cz.msebera.android.httpclient.HttpRequest;
+
 public class Login extends Fragment {
+
+    private String error = "";
+    private EditText email;
+    private EditText password;
+    private String success = "";
+    private String user = "";
+    private String requestBody;
 
     @Override
     public View onCreateView(
@@ -22,13 +48,54 @@ public class Login extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        email = view.findViewById(R.id.email_box);
+        password = view.findViewById(R.id.password_box);
         view.findViewById(R.id.login_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(Login.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                try {
+                    login();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
+
+    public void login() throws UnsupportedEncodingException {
+        HttpEntity loginEntity = new StringEntity(requestBody);
+        HttpUtils.post(null, "buyer/login", loginEntity, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    user = response.getString("email");
+                    toMain();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    error += errorResponse.get("message").toString();
+                } catch (JSONException e) {
+                    error += e.getMessage();
+                }
+            }
+        });
+
+    }
+
+    public void toMain(){
+        Intent i = new Intent(getActivity(), MainActivity.class);
+        startActivity(i);
+        ((Activity) getActivity()).overridePendingTransition(0, 0);
+    }
+
+
+
+
 }
